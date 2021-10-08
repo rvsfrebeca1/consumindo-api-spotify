ce87fbbc457f72b0294ed7271452d4b275cfa3cd
rvsfrebeca1 <rebecaferreiraprofissional@gmail.com>
2021-10-08 10:08:38 -0300

    Apllicação que consome api do spotify

--- classe-01/src/utils/getSpotifyToken.js
+++ classe-01/src/utils/getSpotifyToken.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = '287a9e325446423ea74a184d2b532384'
-const CLIENT_SECRET = '96b28e26f58048508d5f10a7ea4385a3'
+const CLIENT_ID = '//'
+const CLIENT_SECRET = '//'
 
 const baseURL = (id, secret) => `https://accounts.spotify.com/api/token?grant_type=client_credentials&client_id=${id}&client_secret=${secret}`
 
